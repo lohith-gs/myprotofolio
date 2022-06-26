b98741e30fbba04a650a12e5c8679c997c123094
lohith <lohith7711@gmail.com>
2022-06-26 16:58:39 +0530

    intro changes

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -1,30 +1,12 @@
-import React,{useState,useEffect} from "react";
+import React from "react";
 import Typewriter from "typewriter-effect";
 
 import "./Intro.css"
  const Intro=()=>{
-    const text_shuffle=['Namaste!','Hello!','Yo!'];
-    const [greet,setGreet]=useState(0);
-
-    useEffect(() => {
-        const timer = () => {
-          setGreet(prevIndex => {
-            if(prevIndex === text_shuffle.length - 1){
-              return 0;
-            } 
-            return prevIndex + 1;
-          })
-        };
-        setInterval(timer, 2000);
-        
-        
-        return () => { clearInterval(timer); }
-      });
-
     
     return(
         <div className="intro-main">
-            <p className="greet-style">{text_shuffle[greet]}</p>
+            <p className="greet-style">Hello !</p>
             <p className="name-style">I'm Lohith GS.</p>
             <div className="description">
                 <Typewriter
